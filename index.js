0e4890c2c08fd352936a1fb675aa83b5cf15dac1
priyanshjain117 <jainpriyanshu117@gmail.com>
2024-08-05 05:28:28 +0530

    fil

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
 const path = require("path");
-const sendmail=require(".controllers/sendMail");
+const sendmail=require("/controllers/sendMail");
 
 let email_id=null;
 let login_bool=true;
